04ba5558728ac47bb5f70211c55d6972379c483a
Glenen <422624843@qq.com>
2017-03-31 00:28:19 +0800

    (+*+):orientDB图库搭建

--- common/src/main/java/com/woodpecker/acm/tree/BinaryTreeTraversal.java
+++ common/src/main/java/com/woodpecker/acm/tree/BinaryTreeTraversal.java
@@ -1,5 +1,9 @@
 package com.woodpecker.acm.tree;
 
+import lombok.Data;
+
+import java.util.Stack;
+
 /**
  * 二叉树的借助栈结构、及神级方法的先中后序遍历
  *
@@ -7,4 +11,41 @@ package com.woodpecker.acm.tree;
  * @since 2017-03-25
  */
 public class BinaryTreeTraversal {
+
+    /**
+     * 二叉树先序遍历：借助栈和神级遍历
+     *
+     * @param head 头节点
+     */
+    public void preTraversal(Node head) {
+        if (head == null) {
+            return;
+        }
+        Stack<Node> temp = new Stack<>();
+        while (head != null && !temp.isEmpty()) {
+            temp.push(head);
+            System.out.println(head.getDataValue());
+            head = head.left;
+
+        }
+    }
+
+    /**
+     * 定义二叉树
+     */
+    @Data
+    private class Node {
+        /**
+         * 节点值
+         */
+        private int dataValue;
+        /**
+         * 左孩子节点
+         */
+        private Node left;
+        /**
+         * 右孩子节点
+         */
+        private Node right;
+    }
 }
