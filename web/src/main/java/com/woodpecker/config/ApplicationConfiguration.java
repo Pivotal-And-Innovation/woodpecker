7a25bd534cccd8beb9e92150725c062f7ad5653e
shibing.zhao <shibing.zhao@beebank.com>
2017-03-30 18:56:14 +0800

    (+*+)：parent pom文件重构

--- web/src/main/java/com/woodpecker/config/ApplicationConfiguration.java
+++ web/src/main/java/com/woodpecker/config/ApplicationConfiguration.java
@@ -1,10 +1,20 @@
 package com.woodpecker.config;
 
+import com.woodpecker.ApplicationMain;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Controller;
+
 /**
- * 项目配置
+ * 项目配置-扫描注册根目录下所有的bean，同时配置一些小规模零散的bean
  *
  * @author Glenn
  * @since 2017-03-29
  */
+@Configuration
+@ComponentScan(basePackageClasses = {ApplicationMain.class}, excludeFilters = @ComponentScan.Filter({Controller.class,
+        Configuration.class}))
+//@ImportResource(value = "classpath:spring/mybatis-config.xml")
 class ApplicationConfiguration {
 }
